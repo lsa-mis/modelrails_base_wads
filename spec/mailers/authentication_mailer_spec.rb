50f5af0e2dd6d336112b986f48917a8dbd744ac2
Dave Chmura <dschmura@humbledaisy.com>
2026-03-26 19:05:30 -0400

    test: add mailer specs for password_reset_email and invitation details

--- spec/mailers/authentication_mailer_spec.rb
+++ spec/mailers/authentication_mailer_spec.rb
@@ -17,4 +17,26 @@ RSpec.describe AuthenticationMailer, type: :mailer do
       expect(mail.body.encoded).to include(authentication.verification_token)
     end
   end
+
+  describe "#password_reset_email" do
+    let(:user) { create(:user) }
+
+    it "sends to the user's email" do
+      mail = described_class.password_reset_email(user)
+      expect(mail.to).to eq([user.email_address])
+    end
+
+    it "includes the reset token in the body" do
+      freeze_time do
+        token = user.password_reset_token
+        mail = described_class.password_reset_email(user)
+        expect(mail.body.encoded).to include(token)
+      end
+    end
+
+    it "has a subject" do
+      mail = described_class.password_reset_email(user)
+      expect(mail.subject).to be_present
+    end
+  end
 end
--- spec/mailers/invitation_mailer_spec.rb
+++ spec/mailers/invitation_mailer_spec.rb
@@ -19,4 +19,23 @@ RSpec.describe InvitationMailer, type: :mailer do
       expect(mail.body.encoded).to include(invitation.invitable.name)
     end
   end
+
+  describe "#invite details" do
+    let(:invitation) { create(:invitation) }
+
+    it "has a subject including workspace name" do
+      mail = described_class.invite(invitation)
+      expect(mail.subject).to include(invitation.invitable.name)
+    end
+
+    it "includes the inviter name in the body" do
+      mail = described_class.invite(invitation)
+      expect(mail.body.encoded).to include(invitation.invited_by.full_name)
+    end
+
+    it "includes the decline link" do
+      mail = described_class.invite(invitation)
+      expect(mail.body.encoded).to include("decline")
+    end
+  end
 end
