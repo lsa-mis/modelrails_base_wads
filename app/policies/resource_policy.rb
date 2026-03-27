3fe049f1576da911c2f431545052258987f5a3d5
Dave Chmura <dschmura@humbledaisy.com>
2026-03-27 06:36:50 -0400

    feat(auth): Add ResourcePolicy with project membership and creator checks

---
+++ app/policies/resource_policy.rb
@@ -0,0 +1,39 @@
+class ResourcePolicy < ApplicationPolicy
+  def index?
+    project_member?
+  end
+
+  def show?
+    project_member?
+  end
+
+  def create?
+    project_membership&.creator? || project_membership&.editor?
+  end
+
+  def update?
+    resource_creator? || project_membership&.creator?
+  end
+
+  def destroy?
+    resource_creator? || project_membership&.creator? || can?("manage_workspace")
+  end
+
+  def reposition?
+    project_membership&.creator? || project_membership&.editor?
+  end
+
+  private
+
+  def project_membership
+    @project_membership ||= record.project.project_memberships.find_by(user: user)
+  end
+
+  def project_member?
+    project_membership.present?
+  end
+
+  def resource_creator?
+    record.created_by == user
+  end
+end
