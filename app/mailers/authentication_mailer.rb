bfa73b1a20ca8dc24036101316704207860a3f41
Dave Chmura <dschmura@humbledaisy.com>
2026-04-04 07:03:18 -0400

    feat: add email change verification and notification mailers
    Verification email sent to new address with 24-hour token link.
    Notification email sent to current address warning about the change.

--- app/mailers/authentication_mailer.rb
+++ app/mailers/authentication_mailer.rb
@@ -19,4 +19,24 @@ class AuthenticationMailer < ApplicationMailer
       subject: t("authentication_mailer.password_reset_email.subject")
     )
   end
+
+  def email_change_verification(user)
+    @user = user
+    @verification_url = account_email_confirmation_url(token: user.pending_email_token)
+
+    mail(
+      to: user.pending_email,
+      subject: t("authentication_mailer.email_change_verification.subject")
+    )
+  end
+
+  def email_change_notification(user)
+    @user = user
+    @new_email = user.pending_email
+
+    mail(
+      to: user.email_address,
+      subject: t("authentication_mailer.email_change_notification.subject")
+    )
+  end
 end
