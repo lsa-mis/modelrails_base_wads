95a26989844c06fc43be39d390fa15b952065108
Dave Chmura <dschmura@humbledaisy.com>
2026-04-19 08:10:30 -0400

    fix: use Rails.cache for magic link rate limiting instead of process-local MemoryStore

--- app/controllers/magic_links_controller.rb
+++ app/controllers/magic_links_controller.rb
@@ -1,10 +1,8 @@
 class MagicLinksController < ApplicationController
   allow_unauthenticated_access
 
-  RATE_LIMIT_STORE = ActiveSupport::Cache::MemoryStore.new
-
   rate_limit to: 5, within: 3.minutes, only: :create,
-    store: RATE_LIMIT_STORE,
+    store: Rails.cache,
     with: -> { redirect_to new_session_path, alert: t("magic_links.create.rate_limited") }
 
   def create
